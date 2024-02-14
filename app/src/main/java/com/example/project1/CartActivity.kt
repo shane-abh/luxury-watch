e49d3a04bd2a6f4c67bbdcb2b1d26f9ef58b0546
Suresh Adhikari <demon@Sureshs-MacBook-Pro-2.local>
2024-02-14 16:21:29 -0500

    cart activity completed

--- app/src/main/java/com/example/project1/CartActivity.kt
+++ app/src/main/java/com/example/project1/CartActivity.kt
@@ -1,11 +1,37 @@
 package com.example.project1
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
 
 class CartActivity : AppCompatActivity() {
+
+    private var adapter: CartAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
+
+        val query = FirebaseDatabase.getInstance().reference.child("productList").child("rolex")
+
+        val options = FirebaseRecyclerOptions.Builder<Cart>().setQuery(query, Cart::class.java).build()
+        System.out.println(query)
+        System.out.println("options")
+
+        adapter = CartAdapter(options)
+
+        val rView:RecyclerView = findViewById(R.id.rView)
+        rView?.layoutManager = LinearLayoutManager(this )
+        rView?.adapter = adapter
+
+    }
+    override fun onStart() {
+        super.onStart()
+        adapter?.startListening()
+    }
+    override fun onStop() {
+        super.onStop()
+        adapter?.stopListening()
     }
 }
